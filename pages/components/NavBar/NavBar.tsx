b1e4df62373d24cb94ea2e8359025e9a0b3296f6
fitch <metneg@gmail.com>
2022-11-19 17:19:15 +0100

    chore(components) create navbar structure

---
+++ pages/components/NavBar/NavBar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// internal import
+import Style from './NavBar.module.scss';
+
+const NavBar = () => {
+    return (
+        <div>NavBar</div>
+    )
+}
+
+export default NavBar
