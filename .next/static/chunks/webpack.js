7e0a2bbc10ed98cbd97afd3daa31181ee00e98a7
fitch <metneg@gmail.com>
2022-11-20 00:59:02 +0100

    feat(discover) add items to menu

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "35b6746e52066fbd"; }
+/******/ 		__webpack_require__.h = function() { return "d42b7d0a61111ea5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/NavBar/Discover/Discover.tsx
+++ components/NavBar/Discover/Discover.tsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import Link from 'next/link';
+
+// internal import
+import Style from './Discover.module.scss';
 
 const Discover = () => {
+
+    const discover = [{
+        name: 'Collection',
+        link: 'collection'
+    }, {
+        name: 'Search',
+        link: 'search'
+    }, {
+        name: 'Author Profile',
+        link: 'author-profile'
+    }, {
+        name: 'NFT Details',
+        link: 'nft-details'
+    }, {
+        name: 'Account Settings',
+        link: 'account-settings'
+    }, {
+        name: 'Connect Wallet',
+        link: 'connect-wallet'
+    }, {
+        name: 'Collection',
+        link: 'collection'
+    }, {
+        name: 'Blog',
+        link: 'blog'
+    }];
+
     return (
-        <div>Discover</div>
+        <div>
+            {discover.map((item, index) => (
+                <div key={index} className={Style.discover}>
+                    <Link href={{ pathname: `${item.link}` }}>
+                        {item.link}
+                    </Link>
+                </div>
+            ))}
+        </div>
     )
 }
 
